0b02fe5c196ad39e3f0016ad2410c76521373e0c
gkiar <gkiar07@gmail.com>
2018-04-07 17:33:31 -0400

    uncommented bookmarklet page loading

--- bookmarklet.js
+++ bookmarklet.js
@@ -41,7 +41,7 @@
 
   var $mymodal= $('<div class="modal fade" id="readingModal" style="width:100%; height:100%;"><div id="readerShell" style="position:absolute; right:0; width:40%; padding: 10px;"><div id="readerContent" style="background-color: rgba(255, 255, 255, 0.95); padding:20px; border-radius: 10px;"></div></div></div>');
   $('body').append($mymodal);
-  //$('#readerContent').load('./index.html #mainform');
+  $('#readerContent').load('./index.html #mainform');
   $('#readingModal').modal({backdrop: true, focus: true, show: true})
 
   $('#readingModal').on('hidden.bs.modal', function () {
