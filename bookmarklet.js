a70a871828a344626200706f603e00f51f6edc31
gkiar <gkiar07@gmail.com>
2018-04-07 22:51:19 -0400

    updated bookmarklet with x, opacity, and timing

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,31 +1,40 @@
 (function() {
-  // get style sheets
-  var sc_jq = document.createElement("script");
-  sc_jq.type = "text/javascript";
-  sc_jq.id = "reader-script";
-  sc_jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
-  document.body.appendChild(sc_jq);
 
-  var sc_bs1 = document.createElement("script");
-  sc_bs1.type = "text/css";
-  sc_bs1.rel = "stylesheet";
-  sc_bs1.id = "reader-script";
-  sc_bs1.href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
-  document.body.appendChild(sc_bs1);
+  function loadJQ () {
+    var sc_jq = document.createElement("script");
+    sc_jq.type = "text/javascript";
+    sc_jq.id = "reader-script";
+    sc_jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+    document.body.appendChild(sc_jq);
+  };
 
-  var sc_bs2 = document.createElement("script");
-  sc_bs2.type = "text/javascript";
-  sc_bs2.id = "reader-script";
-  sc_bs2.src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js";
-  document.body.appendChild(sc_bs2);
+  function loadBS() {
+    var sc_bs1 = document.createElement("script");
+    sc_bs1.type = "text/css";
+    sc_bs1.rel = "stylesheet";
+    sc_bs1.id = "reader-script";
+    sc_bs1.href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+    document.body.appendChild(sc_bs1)
 
-  $('body').prepend('<div class="modal fade" id="readingModal" style="width:100%; height:100%;"><div id="readerShell" style="position:absolute; right:0; min-width:40%; padding: 10px;"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><iframe id="readerContent" src="https://gkiar.me/reading/embedded.html" style="background-color: rgba(255, 255, 255, 0.95); padding:20px; border-radius: 10px; overflow: unset;" width=100% height=675px></iframe></div></div>');
+    var sc_bs2 = document.createElement("script");
+    sc_bs2.type = "text/javascript";
+    sc_bs2.id = "reader-script";
+    sc_bs2.src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js";
+    document.body.appendChild(sc_bs2)
+  };
 
-  $('#readingModal').modal({backdrop: true, focus: true, show: true})
-  $('#readingModal').on('hidden.bs.modal', function () {
-    $("script[id='reader-script']").remove()
-    $("div[id='readingModal']").remove()
-    $("iframe[id='readerContent']").remove()
-  });
+  function createBookmarklet() {
+    $('body').append('<div class="modal fade" id="readingModal" style="width:100%; height:100%;"><div id="readerShell" style="position:absolute; right:0; min-width:40%; padding: 10px; background-colour: rgba(1,1,1,0.95)"><button type="button" class="close" data-dismiss="modal" aria-label="Close" style="position:relative; top:30px; right:15px"><span aria-hidden="true">&times;</span></button><iframe id="readerContent" src="https://gkiar.me/reading/embedded.html" style="background-color: rgba(255, 255, 255, 0.95); padding:20px; border-radius: 10px; overflow: unset;" width=100% height=675px></iframe></div></div>');
+
+    $('#readingModal').modal({backdrop: true, focus: true, show: true})
+    $('#readingModal').on('hidden.bs.modal', function () {
+      $("script[id='reader-script']").remove()
+      $("div[id='readingModal']").remove()
+      $("iframe[id='readerContent']").remove()
+    });
+  };
+
+  loadJQ()
+  window.setTimeout(loadBS, 300)
+  window.setTimeout(createBookmarklet(), 200);
 }());
-
