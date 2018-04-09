600a373aca95f2955abefaf0c37344beb60cf04b
Greg Kiar <gkiar07@gmail.com>
2018-04-09 13:29:38 -0400

    updated token get method

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,6 @@
-(function(token="") {
+(function() {
   var idnum = Math.random().toString(9).substr(2,4)
+  var token = document.getElementById('readerBookmarklet').getAttribute('token')
 
   var readerText = '<div id="readerShell-NUM" style="position:fixed; top:20px; right:20px; min-width:25%; max-width:90%; z-index:2147483648"><button type="button" class="close" id="readerClose-NUM" aria-label="Close" style="position:fixed; top:25px; right:25px"><span aria-hidden="true">&times;</span></button><iframe id="readerContent-NUM" src="https://gkiar.me/reading/embedded.html?name=NAME&url=URL&token=TOKEN" height=640px width=100% style="border:none; border-radius: 10px; background-color:rgba(240,240,240,0.95);"></iframe></div>'
   readerText = readerText.replace(/NUM/g, idnum)
