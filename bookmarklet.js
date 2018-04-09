3c406797b1aa37529f77586049d5a9422dcab660
Greg Kiar <gkiar07@gmail.com>
2018-04-09 12:02:51 -0400

    updated embedded and bookmarklet to enable passing tokens

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,11 @@
-(function() {
+(function(token="") {
   var idnum = Math.random().toString(9).substr(2,4)
 
-  var readerText = '<div id="readerShell-NUM" style="position:fixed; top:20px; right:20px; min-width:25%; max-width:90%; z-index:2147483648"><button type="button" class="close" id="readerClose-NUM" aria-label="Close" style="position:fixed; top:25px; right:25px"><span aria-hidden="true">&times;</span></button><iframe id="readerContent-NUM" src="https://gkiar.me/reading/embedded.html?name=NAME&url=URL" height=620px width=100% style="border:none; border-radius: 10px; background-color:rgba(240,240,240,0.95);"></iframe></div>'
+  var readerText = '<div id="readerShell-NUM" style="position:fixed; top:20px; right:20px; min-width:25%; max-width:90%; z-index:2147483648"><button type="button" class="close" id="readerClose-NUM" aria-label="Close" style="position:fixed; top:25px; right:25px"><span aria-hidden="true">&times;</span></button><iframe id="readerContent-NUM" src="https://gkiar.me/reading/embedded.html?name=NAME&url=URL&token=TOKEN" height=640px width=100% style="border:none; border-radius: 10px; background-color:rgba(240,240,240,0.95);"></iframe></div>'
   readerText = readerText.replace(/NUM/g, idnum)
   readerText = readerText.replace(/NAME/g, window.document.title)
   readerText = readerText.replace(/URL/g, window.location.href)
+  readerText = readerText.replace(/TOKEN/g, token)
   document.body.innerHTML += readerText
 
   var iframeObject = document.getElementById("readerContent-"+idnum);
