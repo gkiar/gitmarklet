107908cc37bc0356d71c76592287f5c17dfe3093
gkiar <gkiar07@gmail.com>
2018-04-09 19:44:02 -0400

    updated bookmarklet to enable arbitrary repo submissions

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,12 +1,14 @@
 (function() {
   var idnum = Math.random().toString(9).substr(2,4)
   var token = document.getElementById('readerBookmarklet').getAttribute('token')
+  var repo = document.getElementById('readerBookmarklet').getAttribute('repo')
 
-  var readerText = '<div id="readerShell-NUM" style="position:fixed; top:20px; right:20px; min-width:25%; max-width:90%; z-index:2147483648"><button type="button" class="close" id="readerClose-NUM" aria-label="Close" style="position:fixed; top:25px; right:25px"><span aria-hidden="true">&times;</span></button><iframe id="readerContent-NUM" src="https://gkiar.me/reading/embedded.html?name=NAME&url=URL&token=TOKEN" height=640px width=100% style="border:none; border-radius: 10px; background-color:rgba(240,240,240,0.95);"></iframe></div>'
+  var readerText = '<div id="readerShell-NUM" style="position:fixed; top:20px; right:20px; min-width:25%; max-width:90%; z-index:2147483648"><button type="button" class="close" id="readerClose-NUM" aria-label="Close" style="position:fixed; top:25px; right:25px"><span aria-hidden="true">&times;</span></button><iframe id="readerContent-NUM" src="https://gkiar.me/reading/embedded.html?name=NAME&url=URL&token=TOKEN&repo=REPO" height=680px width=100% style="border:none; border-radius: 10px; background-color:rgba(240,240,240,0.95);"></iframe></div>'
   readerText = readerText.replace(/NUM/g, idnum)
   readerText = readerText.replace(/NAME/g, window.document.title)
   readerText = readerText.replace(/URL/g, window.location.href)
   readerText = readerText.replace(/TOKEN/g, token)
+  readerText = readerText.replace(/REPO/g, repo)
   document.body.innerHTML += readerText
 
   var iframeObject = document.getElementById("readerContent-"+idnum);
