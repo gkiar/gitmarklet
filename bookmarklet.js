7ddb3aa4dc37edad231d63eff8ef2062648d3e68
gkiar <gkiar07@gmail.com>
2018-04-25 12:35:18 -0400

    updated height to not exceed page height

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,7 +3,7 @@
   var token = document.getElementById('readerBookmarklet').getAttribute('token')
   var repo = document.getElementById('readerBookmarklet').getAttribute('repo')
 
-  var readerText = '<div id="readerShell-NUM" style="position:fixed; top:20px; right:20px; min-width:25%; max-width:90%; z-index:2147483648"><button type="button" class="close" id="readerClose-NUM" aria-label="Close" style="position:fixed; top:25px; right:25px"><span aria-hidden="true">&times;</span></button><iframe id="readerContent-NUM" src="https://gkiar.me/reading/embedded.html?name=NAME&url=URL&token=TOKEN&repo=REPO" height=795px width=100% style="border:none; border-radius: 10px; background-color:rgba(240,240,240,0.95)"></iframe></div>'
+  var readerText = '<div id="readerShell-NUM" style="position:fixed; top:20px; right:20px; min-width:25%; max-width:90%; z-index:2147483648"><button type="button" class="close" id="readerClose-NUM" aria-label="Close" style="position:fixed; top:25px; right:25px"><span aria-hidden="true">&times;</span></button><iframe id="readerContent-NUM" src="https://gkiar.me/reading/embedded.html?name=NAME&url=URL&token=TOKEN&repo=REPO" height=95% width=100% style="border:none; border-radius: 10px; background-color:rgba(240,240,240,0.95)"></iframe></div>'
   readerText = readerText.replace(/NUM/g, idnum)
   readerText = readerText.replace(/NAME/g, window.document.title)
   readerText = readerText.replace(/URL/g, window.location.href)
